482604fa3b650d2463bfb460fb928616a016ad07
Henrique Müller <hm@hellofresh.com>
2018-03-07 17:25:46 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './global.css';
+import Root from './modules/Root';
+
+ReactDOM.render(<Root />, document.getElementById('root'));
